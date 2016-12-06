00110f8022709933a55495047e7ebc7bde36ea37
Brandon Perez <bmperez@andrew.cmu.edu>
2016-12-06 00:21:47 -0500

    Increased the learning factor for gradient descent optimization.
    It was increased by a factor of 5, this gives better overall reults on
    images.

--- train.py
+++ train.py
@@ -219,7 +219,7 @@ else:
     loss = (tf.split(3, 2, loss)[0] + tf.split(3, 2, loss)[1]) / 2
 
 if phase_train is not None:
-    optimizer = tf.train.GradientDescentOptimizer(0.0001)
+    optimizer = tf.train.GradientDescentOptimizer(5*0.0001)
     opt = optimizer.minimize(
         loss, global_step=global_step, gate_gradients=optimizer.GATE_NONE)
 
