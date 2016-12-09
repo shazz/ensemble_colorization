86e9f3d93ad37063082d281adaf703e51b570e85
Brandon Perez <bmperez@andrew.cmu.edu>
2016-12-08 20:53:15 -0500

    Reduced the number of jobs for the resize script parallelization.
    Image manipulation is compute/memory bound, so it doesn't make sense to
    have a ton of jobs. Instead twice the number of cores are spawned.

--- resize.sh
+++ resize.sh
@@ -12,7 +12,6 @@ set -o pipefail
 
 # Program usage, number of parallel processes to spawn, and output image size
 USAGE="./resize.sh <image_dir> <output_dir>"
-NUM_JOBS=1024
 IMAGE_SIZE=256x256
 
 # Check that the number of command line arguments is valid
@@ -31,5 +30,7 @@ output_dir=$2
 mkdir -p ${output_dir}
 
 # Convert images in parallel, using as many processes as possible
-find -L ${image_dir} -type f -name '*.jpg' | parallel -j${NUM_JOBS} --progress \
+num_cores=$(getconf _NPROCESSORS_ONLN)
+num_jobs=$((2 * ${num_cores}))
+find -L ${image_dir} -type f -name '*.jpg' | parallel -j${num_jobs} --progress \
         convert -resize ${IMAGE_SIZE}\! {} ${output_dir}/{/}
