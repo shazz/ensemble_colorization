33541f3da4ae83efda4b173390bbc2cbea5754fd
Brandon Perez <bmperez@andrew.cmu.edu>
2016-12-09 02:29:07 -0500

    Extended the testing script to accept a directory of testing images.
    The script will now iterate over and process all images in the specified
    directory. Also, the results will be stored in the specified output
    directory, under the same filename.

--- test.py
+++ test.py
@@ -1,15 +1,22 @@
 #! /usr/bin/python
 
+import os
+import glob
 import tensorflow as tf
-import sys
 from matplotlib import pyplot as plt
-import skimage
-import skimage.io
 import numpy as np
+from argparse import ArgumentParser
 
-filename = sys.argv[1]
-phase_train = tf.placeholder(tf.bool, name='phase_train')
-uv = tf.placeholder(tf.uint8, name='uv')
+def parse_arguments():
+    parser = ArgumentParser(description="Runs the testing phase of image "
+            "recolorizationm running the trained network on the list of "
+            "testing images, saving it the specified output directory.")
+    parser.add_argument("image_dir", type=str, help="The directory "
+        "containing the JPEG images to run testing on.")
+    parser.add_argument("output_dir", type=str, help="The output directory to "
+        "place the results of testing into. The results are the grayscale, "
+        "test result, and original images concatenated together.")
+    return parser.parse_args()
 
 def concat_images(imga, imgb):
     """
@@ -72,54 +79,74 @@ def recombine(predictions):
     sum_image = red_biased + blue_biased + green_biased
     return sum_image / 3.
 
-with tf.Session() as sess:
-    saver = tf.train.import_meta_graph('model_blue.meta')
+def main():
+    args = parse_arguments()
 
-    predictions = dict()
+    # Create the output directory if it does not exist
+    if not os.path.exists(args.image_dir):
+        os.mkdir(args.image_dir)
 
-    for color in ['red', 'green', 'blue']:
-        print 'Restoring session...'
-        saver.restore(sess, 'model_%s' % color)
-        print 'Session loaded!'
+    phase_train = tf.placeholder(tf.bool, name='phase_train')
+    uv = tf.placeholder(tf.uint8, name='uv')
 
-        graph = tf.get_default_graph()
-        print 'Loaded default graph'
+    with tf.Session() as sess:
+        saver = tf.train.import_meta_graph('model_blue.meta')
 
-        print 'Processing image %s ...' % filename
+        image_paths = glob.glob(os.path.join(args.image_dir, "*.jpg"))
+        for image_path in image_paths:
+            predictions = dict()
 
-        contents = tf.read_file(filename)
-        uint8image = tf.image.decode_jpeg(contents, channels=3)
-        resized_image = tf.div(tf.image.resize_images(uint8image, (224, 224)), 255)
-        img = sess.run(resized_image)
+            for color in ['red', 'green', 'blue']:
+                print 'Restoring session...'
+                saver.restore(sess, 'model_%s' % color)
+                print 'Session loaded!'
 
-        print'Done processing image!'
+                graph = tf.get_default_graph()
+                print 'Loaded default graph'
 
-        pred = graph.get_tensor_by_name("colornet_1/conv2d_4/Sigmoid:0")
+                print 'Processing image %s ...' % image_path
 
-        grayscale = tf.image.rgb_to_grayscale(resized_image)
-        grayscale = tf.reshape(grayscale, [1, 224, 224, 1])
-        grayscale_rgb = tf.image.grayscale_to_rgb(grayscale)
-        grayscale_yuv = rgb2yuv(grayscale_rgb)
-        grayscale = tf.concat(3, [grayscale, grayscale, grayscale])
+                contents = tf.read_file(image_path)
+                uint8image = tf.image.decode_jpeg(contents, channels=3)
+                resized_image = tf.div(tf.image.resize_images(uint8image, (224, 224)), 255)
+                img = sess.run(resized_image)
 
-        print 'done transforms'
+                print'Done processing image!'
 
-        pred_yuv = tf.concat(3, [tf.split(3, 3, grayscale_yuv)[0], pred])
-        pred_rgb = yuv2rgb(pred_yuv)
+                pred = graph.get_tensor_by_name("colornet_1/conv2d_4/Sigmoid:0")
 
-        input_image = sess.run(grayscale)
+                grayscale = tf.image.rgb_to_grayscale(resized_image)
+                grayscale = tf.reshape(grayscale, [1, 224, 224, 1])
+                grayscale_rgb = tf.image.grayscale_to_rgb(grayscale)
+                grayscale_yuv = rgb2yuv(grayscale_rgb)
+                grayscale = tf.concat(3, [grayscale, grayscale, grayscale])
 
-        feed_dict = {phase_train : False, uv: 3, graph.get_tensor_by_name('concat:0') : input_image}
+                print 'done transforms'
 
-        print 'Running colornet...'
-        pred_, pred_rgb_, colorimage_, grayscale_rgb_ = sess.run(
-            [pred, pred_rgb, resized_image, grayscale_rgb], feed_dict=feed_dict)
+                pred_yuv = tf.concat(3, [tf.split(3, 3, grayscale_yuv)[0], pred])
+                pred_rgb = yuv2rgb(pred_yuv)
 
-        predictions[color] = pred_rgb_[0]
+                input_image = sess.run(grayscale)
 
-    # Combine the three color-baised images into a final response
-    output = recombine(predictions)
+                feed_dict = {phase_train : False, uv: 3, graph.get_tensor_by_name('concat:0') : input_image}
 
-    image = concat_images(grayscale_rgb_[0], output)
-    image = concat_images(image, img)
-    plt.imsave("output.png", image)
+                print 'Running colornet...'
+                pred_, pred_rgb_, colorimage_, grayscale_rgb_ = sess.run(
+                    [pred, pred_rgb, resized_image, grayscale_rgb], feed_dict=feed_dict)
+
+                predictions[color] = pred_rgb_[0]
+
+            # Combine the three color-baised images into a final response
+            output = recombine(predictions)
+
+            # Concatenate the grayscale, result, and original images together
+            output_image = concat_images(grayscale_rgb_[0], output)
+            output_image = concat_images(output_image, img)
+
+            # Save the output image to the directory with the same name
+            image_name = os.path.basename(image_path)
+            output_image_path = os.path.join(args.output_dir, image_name)
+            plt.imsave(output_image_path, output_image)
+
+if __name__ == '__main__':
+    main()
